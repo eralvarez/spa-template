5558babbb9bd15604700897ca1c0eda7b436f395
Erick Alvarez <erick.alvarez@live.com>
2026-09-05 07:45:10 -0700

    generouted support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import generouted from '@generouted/react-router/plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+        babel({ presets: [reactCompilerPreset()] }),
+        generouted()
+
   ],
 })
