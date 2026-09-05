00ff69f37eb710cd3fc9c7c41112ee2f6c0f06da
Erick Alvarez <erick.alvarez@live.com>
2026-09-05 07:47:51 -0700

    prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
-import generouted from '@generouted/react-router/plugin'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
+import { defineConfig } from 'vite';
+import generouted from '@generouted/react-router/plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-        babel({ presets: [reactCompilerPreset()] }),
-        generouted()
-
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), generouted()],
+});
